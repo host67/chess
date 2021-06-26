9465558d368ac534835964ed05299e5304b28f75
Alex <host67@yandex.ru>
2021-06-26 19:53:51 +0300

    Limit of input, without validation

--- index.php
+++ index.php
@@ -1,56 +1,55 @@
 <!DOCTYPE html>
 <html> 
-<head> 
-    <title></title>
+<head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <title>Шахматная доска</title>
 </head>
 <body> 
-<h3>Шахматная доска с использованием цикла For</h3>
+<h3>Шахматная доска</h3>
+<?php 
+$height = $width = 8;
+if ($_POST['height']) $height = $_POST['height'];
+if ($_POST['width']) $width = $_POST['width'];
+?>
 <form action="" method="post">
-    Высота: <input type="text" name="height" placeholder="8">
-    Ширина: <input type="text" name="widht" placeholder="8">
+    Высота: <input type="number" min="1" max="20" step="1" name="height" placeholder="<?php echo $height ?>">
+    Ширина: <input type="number" min="1" max="20" step="1" name="width" placeholder="<?php echo $width ?>">
     <button type="submit">Выполнить</button> 
 </form>
 <br>
-<table style="max-width:100%;text-align:center" cellspacing="0px" cellpadding="0px" border="1px">
+<table cellspacing="0px" cellpadding="0px" border="1px">
 <?php
-$height = $width = 8;
-if ($_POST['height']) $height = $_POST['height'];
-if ($_POST['widht']) $width = $_POST['widht'];
-
-$alfabet = array("A","B","C","D","E","F","G","H","I","J", "K", "L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z");
-$caption_width = [];
-
-while (count($caption_width) < $width) {
-    $caption_width = array_merge($caption_width, $alfabet);
-}
-
+$caption_width = array("A","B","C","D","E","F","G","H","I","J", "K", "L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z");
 array_unshift( $caption_width, "");
 
+$chess = '';
+
 for($row=1;$row<=$height;$row++) {
-    echo '<tr><td style="border:none;width:25px">'.$row.'</td>';
+    $chess .= '<tr><td class="caption-height">'.$row.'</td>';
     for($col=1;$col<=$width;$col++) {
         $total = $row+$col;
         if($total%2==0) {
-            echo "<td height=25px width=25px bgcolor=#FFFFFF></td>";
+            $chess .= '<td class="td-white"></td>';
         } else {
-        echo "<td height=25px width=25px bgcolor=#000000></td>";
+        $chess .= '<td class="td-black"></td>';
         }
     }
-    echo "</tr>";
+    $chess .= '</tr>';
 }
 
-echo '<tr>';
+$chess .= '<tr>';
 
 for($col2=0;$col2<=$width;$col2++) {
     if ($col2 == 0) {
-        echo '<td style="border:none;"></td>';
+        $chess .= '<td class="no-border"></td>';
     } else {
-        echo '<td style="border:none;">'.$caption_width[$col2].'</td>';
+        $chess .= '<td class="no-border">'.$caption_width[$col2].'</td>';
     }
 }
 
-echo '</tr>';
+$chess .= '</tr>';
+echo $chess;
 
 function debug($arr) {
     echo "<pre>" . print_r($arr, true) . "</pre>";
